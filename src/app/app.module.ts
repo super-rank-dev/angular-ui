33ee9a1ab6e16753391cf9f43298fb64fbb48836
paddy0417 <patrick@gensy.com>
2023-07-17 21:45:19 -0400

    angular dashboard completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LandingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
